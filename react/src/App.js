18201acdd078f59e3dbab0c5ca8a62fe49a790e2
hejianxian <531601727@qq.com>
2020-10-18 22:28:14 +0800

    chore: add react demo

--- react/src/App.js
+++ react/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import * as w6s from '@w6s/sdk';
+
 import './App.css';
 
+w6s.header.setTitle('JS-SDK ReactJS Demo');
+
 function App() {
+  const [device, setDeviceInfo] = useState('123');
+  useEffect(() => {
+    w6s.device.getDeviceInfo()
+      .then(res => {
+        setDeviceInfo(JSON.stringify(res.result, null, 4));
+      });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +31,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <h3>设备信息：</h3>
+      <p>{device}</p>
     </div>
   );
 }
